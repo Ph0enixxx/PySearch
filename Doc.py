b3cede8d5b279a22942aa12db6506d464a968331
shaoyingshuai <754406613@qq.com>
2016-10-05 19:41:47 +0800

    fix a var renamed bug

--- Doc.py
+++ Doc.py
@@ -18,25 +18,33 @@ class Doc(object):
 	def save(self,doc=""):
 		data_ = doc
 		#每个非空词 先获取 再提交
+		print(data_)
 		for i in data_:
 			num  = i[0]
 			data = i[1]
 			fun.msg(num,2)
 			fun.msg(data,2)
+
 			for x in data:
-				if x != "":
+				if x != "" and x != " ":
 					#获取json 转换 添加 写回
 					##try catch
+					print(type(x))
+					#fun.msg(x,1)
+					print(x)
 					js = self.data.get(x)
 					fun.msg(js,2)
-					# if js == None:
-					# 	num = self.data.json_encode([num])
-					# 	self.data.set(x,num)
-					# else:
-					# 	js = list(self.data.json_decode(js))
-					# 	js.append(num)
-					# 	num = self.data.json_encode(js)
-					# 	self.data.set(x,num)
+					if js == None:
+						l = [num]
+						fun.msg(l,2)
+						numx = self.data.json_encode(l)
+						fun.msg(numx)
+						self.data.set(x,numx)
+					else:
+						js = list(self.data.json_decode(js))
+						js.append(num)
+						numx = self.data.json_encode(js)
+						self.data.set(x,numx)
 		#分词
 			#判断空
 		#每个都提交数据库
@@ -49,7 +57,7 @@ class Doc(object):
 	def getData(self):
 		s = Sql("127.0.0.1","root","","oj")
 		try:
-			p = [(i[0],Cut.go(i[1]))for i in s.getStringResult("select * from oj_contest")]
+			p = [(i[0],Cut.go(i[1]))for i in s.getStringResult("select * from problem")]
 		#print(p)
 			return p
 		except:
