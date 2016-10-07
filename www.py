730d46eaba4aee1b1481cf499c5ff7d1fee17744
shaoyingshuai <754406613@qq.com>
2016-10-07 14:32:05 +0800

    add web UI

--- www.py
+++ www.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 from Doc import Doc
 #提供接口
 app = Flask(__name__)
 doc = Doc()
 
+
+#前端页面
+#搜索关键词后，出现符合条件的题号？
 @app.route("/")
 def hello():
-	return "index"
+	return render_template("index.html")
 
 @app.route("/key/<keyword>")
 def search(keyword):
@@ -14,3 +17,6 @@ def search(keyword):
 
 if __name__ == '__main__':
 	app.run(debug=True,port=8888,host="0.0.0.0")
+
+
+#部署
